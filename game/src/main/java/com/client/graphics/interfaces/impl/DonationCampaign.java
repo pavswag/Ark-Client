f75aae4f22b4ea3d7801800f5053acf61287c12a
i-iz-adam <96274526+i-iz-adam@users.noreply.github.com>
2024-06-17 19:11:32 +0100

    Update DonationCampaign.java

--- game/src/main/java/com/client/graphics/interfaces/impl/DonationCampaign.java
+++ game/src/main/java/com/client/graphics/interfaces/impl/DonationCampaign.java
@@ -55,35 +55,36 @@ public class DonationCampaign extends RSInterface {
 
         int baseX = 45, baseY = 8;
 
-        main.child(index++, interfaceId + index, baseX, baseY); //background
-        main.child(index++, interfaceId + index, baseX + 220, baseY + 10); //title
-        main.child(index++, interfaceId + index, 405 + baseX, 10 + baseY); //close button
-        main.child(index++, interfaceId + index, 75 + baseX, 45 + baseY); //deals button
-        main.child(index++, interfaceId + index, 75 + baseX + 98, 45 + baseY); //store button
-        main.child(index++, interfaceId + index, 75 + baseX + 98 + 98, 45 + baseY); //campaign button
-        main.child(index++, interfaceId + index, baseX + 17, 82 + baseY); //main background
-        main.child(index++, interfaceId + index, baseX + 220, 82 + baseY); //main background
-        main.child(index++, interfaceId + index, baseX + 17, 197 + baseY); //main background
-        main.child(index++, interfaceId + index, baseX + 220, 197 + baseY); //main background
-        main.child(index++, interfaceId + index, baseX + 17+ 99, 87 + baseY); //Prize #1
-        main.child(index++, interfaceId + index, baseX + 220 + 99, 87 + baseY); //Prize #2
-        main.child(index++, interfaceId + index, baseX + 17 + 99, 202 + baseY); //Prize #3
-        main.child(index++, interfaceId + index, baseX + 220 + 99, 202 + baseY); //Prize #4
-        main.child(index++, interfaceId + index, baseX + 23, 162 + baseY); //Progress bar bg #1
-        main.child(index++, interfaceId + index, baseX + 226 , 162 + baseY); //Progress bar bg #2
-        main.child(index++, interfaceId + index, baseX + 23, 277 + baseY); //Progress bar bg  #3
-        main.child(index++, interfaceId + index, baseX + 226, 277 + baseY); //Progress bar bg #4
-        main.child(index++, interfaceId + index, baseX + 23, 162 + baseY); //Progress bar #1
-        main.child(index++, interfaceId + index, baseX + 226 , 162 + baseY); //Progress bar #2
-        main.child(index++, interfaceId + index, baseX + 23, 277 + baseY); //Progress bar #3
-        main.child(index++, interfaceId + index, baseX + 226, 277 + baseY); //Progress bar #4
-        main.child(index++, interfaceId + index, baseX + 27, 162 - 49 + baseY); //item #1
-        main.child(index++, interfaceId + index, baseX + 230, 162 - 49 + baseY); //item #2
-        main.child(index++, interfaceId + index, baseX + 27, 277 - 49 + baseY); //item #3
-        main.child(index++, interfaceId + index, baseX + 230, 277 - 49 + baseY); //item #4
-        main.child(index++, interfaceId + index, baseX + 23 + 92, 162 + 6 + baseY); //progress text #1
-        main.child(index++, interfaceId + index, baseX + 226 + 92, 162 + 6 + baseY); //progress text #2
-        main.child(index++, interfaceId + index, baseX + 23 + 92, 277 + 6 + baseY); //progress text #3
-        main.child(index++, interfaceId + index, baseX + 226 + 92, 277 + 6 + baseY); //progress text #4
+        main.child(0, 24231, baseX, baseY); //background
+        main.child(1, 24232, baseX + 220, baseY + 10); //title
+        main.child(2, 24233, 405 + baseX, 10 + baseY); //close button
+        main.child(3, 24235, 75 + baseX, 45 + baseY); //deals button
+        main.child(4, 24237, 75 + baseX + 98, 45 + baseY); //store button
+        main.child(5, 24239, 75 + baseX + 98 + 98, 45 + baseY); //campaign button
+        main.child(6, 24240, baseX + 17, 82 + baseY); //main background
+        main.child(7, 24241, baseX + 220, 82 + baseY); //main background
+        main.child(8, 24242, baseX + 17, 197 + baseY); //main background
+        main.child(9, 24243, baseX + 220, 197 + baseY); //main background
+        main.child(10, 24244, baseX + 17 + 99, 87 + baseY); //Prize #1
+        main.child(11, 24245, baseX + 220 + 99, 87 + baseY); //Prize #2
+        main.child(12, 24246, baseX + 17 + 99, 202 + baseY); //Prize #3
+        main.child(13, 24247, baseX + 220 + 99, 202 + baseY); //Prize #4
+        main.child(14, 24248, baseX + 23, 162 + baseY); //Progress bar bg #1
+        main.child(15, 24249, baseX + 226, 162 + baseY); //Progress bar bg #2
+        main.child(16, 24250, baseX + 23, 277 + baseY); //Progress bar bg #3
+        main.child(17, 24251, baseX + 226, 277 + baseY); //Progress bar bg #4
+        main.child(18, 24252, baseX + 23, 162 + baseY); //Progress bar #1
+        main.child(19, 24253, baseX + 226, 162 + baseY); //Progress bar #2
+        main.child(20, 24254, baseX + 23, 277 + baseY); //Progress bar #3
+        main.child(21, 24255, baseX + 226, 277 + baseY); //Progress bar #4
+        main.child(22, 24256, baseX + 27, 162 - 49 + baseY); //item #1
+        main.child(23, 24257, baseX + 230, 162 - 49 + baseY); //item #2
+        main.child(24, 24258, baseX + 27, 277 - 49 + baseY); //item #3
+        main.child(25, 24259, baseX + 230, 277 - 49 + baseY); //item #4
+        main.child(26, 24260, baseX + 23 + 92, 162 + 6 + baseY); //progress text #1
+        main.child(27, 24261, baseX + 226 + 92, 162 + 6 + baseY); //progress text #2
+        main.child(28, 24262, baseX + 23 + 92, 277 + 6 + baseY); //progress text #3
+        main.child(29, 24263, baseX + 226 + 92, 277 + 6 + baseY); //progress text #4
+
     }
 }
